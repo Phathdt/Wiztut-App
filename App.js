3ab94142433c3cd5eb51d669eaa2aaa1ccb04e21
Phathdt <Phathdt.leviateam@gmail.com>
2017-10-07 00:53:03 +0700

    Change source and update to 0.49

--- App.js
+++ App.js
@@ -1,57 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  Platform,
+  AppRegistry,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import  { SignedIn, SignedOut, createRootNavigator } from './source/Router'
 
-export default class App extends Component<{}> {
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      signedIn: false,
+    };
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+    const { checkedSignIn, signedIn } = this.state;
+
+    const Layout = createRootNavigator(signedIn);
+    return <Layout />;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+AppRegistry.registerComponent('Wiztut', () => App);
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-import App from './App/App';
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('Wiztut', () => App);
