401a6c2f6ee803ec0a6b31c98cc6d15699f40628
Tạ Thành Việt Anh <tavietanh2007@gmail.com>
2017-10-14 00:22:21 +0700

    signin, signup

--- source/Screens/SignedOut/SignIn.js
+++ source/Screens/SignedOut/SignIn.js
@@ -1,20 +1,66 @@
 import React, { Component } from 'react';
 
-import { View, Button } from 'react-native';
+import { View, Button,Text,TextInput } from 'react-native';
 
 export default class SignIn extends Component {
+  constructor(props){
+    super(props);
+    this.state = {email: '',password: ''};
+    this.ClickMe = this.ClickMe.bind(this)
+    this.getAPI=this.getAPI.bind(this)
+  }
+  ClickMe() {
+    this.getAPI();
+  }
   render() {
     return (
       <View>
         <Button
           title="SignedIn"
-          onPress={() => this.props.navigation.navigate("SignedIn")}
+          onPress={this.ClickMe}
+        />
+        <Text>
+          Email:
+        </Text>
+        <TextInput
+          value = {this.state.email}
+          onChangeText={(email) => this.setState({email})}
+        />
+        <Text>
+          password:
+        </Text>
+        <TextInput
+          value = {this.state.password}
+          onChangeText={(password) => this.setState({password})}
         />
         <Button
-          title="SignUp"
-          onPress={() => this.props.navigation.navigate("SignUp")}
+          title="Sign up"
+          onPress={()=> this.props.navigation.navigate("SignUp")}
         />
       </View>
-    );
+    )
+  }
+  async getAPI(){
+    if(this.state.email!=''&& this.state.password!=''){
+    try {
+      let response = await fetch('https://mysterious-shore-50693.herokuapp.com/api/v1/users/sign_in', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          session:{
+            email: this.state.email,
+            password: this.state.password
+          }
+        })
+      });
+      if(response.status==200){
+        this.props.navigation.navigate("SignedIn");
+      }
+    } catch(error) {
+      
+      }
+    }
   }
 }
--- source/Screens/SignedOut/SignUp.js
+++ source/Screens/SignedOut/SignUp.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 
-import { View, Button } from 'react-native';
+import { View, Button,Text,TextInput } from 'react-native';
 
 export default class SignUp extends Component {
+  constructor(props){
+    super(props);
+    this.state = {email: '',password: '',re_password:''};
+    this.ClickMe = this.ClickMe.bind(this)
+    this.getAPI=this.getAPI.bind(this)
+  }
+  ClickMe() {
+    this.getAPI();
+  }
   render() {
     return (
       <View>
@@ -10,7 +19,61 @@ export default class SignUp extends Component {
           title="SignedIn"
           onPress={() => this.props.navigation.navigate("SignedIn")}
         />
+        <Text>
+          Email:
+        </Text>
+        <TextInput
+          value = {this.state.email}
+          onChangeText={(email) => this.setState({email})}
+        />
+        <Text>
+          password:
+        </Text>
+        <TextInput
+          value = {this.state.password}
+          onChangeText={(password) => this.setState({password})}
+        />
+        <Text>
+          Re-password:
+        </Text>
+        <TextInput
+          value = {this.state.re_password}
+          onChangeText={(re_password) => this.setState({re_password})}
+        />
+        <Button
+          title="Sign up"
+          onPress={this.ClickMe}
+        />
       </View>
+      
     );
   }
+  async getAPI(){
+    if(this.state.password==this.state.re_password && this.state.email!=''&& this.state.password!='')
+    try {
+      let response = await fetch('https://mysterious-shore-50693.herokuapp.com/api/v1/users/sign_up', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          registration:{
+            email: this.state.email,
+            password: this.state.password,
+            password_confirmation:this.state.re_password
+          }
+        })
+      });
+      if(response.status==200){
+        this.props.navigation.navigate("SignedIn");
+      }
+      
+    } catch(error) {
+      
+    }
+  
+  }
+  
 }
+
+
