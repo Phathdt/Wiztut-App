78faaad1e7bf28a548493d9810bbbf7f1d82bed4
Tạ Thành Việt Anh <tavietanh2007@gmail.com>
2017-10-17 00:35:13 +0700

    add feature 28 password

--- source/Screens/SignedOut/SignIn.js
+++ source/Screens/SignedOut/SignIn.js
@@ -7,54 +7,28 @@ import {
   TextInput 
 } from 'react-native';
 
+import {SignInUrl} from '../../helper/LinkUrl';
+
 export default class SignIn extends Component {
   constructor(props){
     super(props);
     this.state = {
-      email     : '',
-      password  : ''
+      email   : '',
+      password: ''
     };
   }
-  render() {
-    return (
-      <View>
-        <Text>
-          Email:
-        </Text>
-        <TextInput
-          value = {this.state.email}
-          onChangeText={(email) => this.setState({email})}
-        />
-        <Text>
-          password:
-        </Text>
-        <TextInput
-          value = {this.state.password}
-          onChangeText={(password) => this.setState({password})}
-        />
-        <Button
-          title="SignedIn"
-          onPress={() => this.ClickSignIn()}
-        />
-        <Button
-          title="Sign up"
-          onPress={() => this.props.navigation.navigate("SignUp")}
-        />
-      </View>
-    )
-  }
-  async ClickSignIn(){
+  async SignIn(){
     if(this.state.email != '' && this.state.password != ''){
       try {
-        let response = await fetch('https://mysterious-shore-50693.herokuapp.com/api/v1/users/sign_in', {
-          method: 'POST',
+        let response = await fetch(SignInUrl, {
+          method : 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
           body: JSON.stringify({
             session:{
-              email     : this.state.email,
-              password  : this.state.password
+              email   : this.state.email,
+              password: this.state.password
             }
           })
         });
@@ -67,4 +41,34 @@ export default class SignIn extends Component {
       }
     }
   }
+  render() {
+    return (
+      <View>
+        <Text>
+          Email: 
+        </Text>
+        <TextInput
+          value        = {this.state.email}
+          onChangeText = {(email) => this.setState({email})}
+        />
+        <Text>
+          password: 
+        </Text>
+        <TextInput
+          value           = {this.state.password}
+          secureTextEntry = {true}
+          onChangeText    = {(password) => this.setState({password})}
+        />
+        <Button
+          title   = "SignedIn"
+          onPress = {() => this.SignIn()}
+        />
+        <Button
+          title   = "Sign up"
+          onPress = {() => this.props.navigation.navigate("SignUp")}
+        />
+      </View>
+    )
+  }
+
 }
--- source/Screens/SignedOut/SignUp.js
+++ source/Screens/SignedOut/SignUp.js
@@ -7,83 +7,85 @@ import {
   TextInput 
 } from 'react-native';
 
+import {SignUpUrl} from '../../helper/LinkUrl';
+
 export default class SignUp extends Component {
   constructor(props){
     super(props);
     this.state = {
-      email                 : '',
-      password              : '',
-      password_confirmation :''
+      email                : '',
+      password             : '',
+      password_confirmation: ''
     };
+  }
+  async SignIn(){
+    if(
+      this.state.password == this.state.password_confirmation
+      && this.state.email != ''
+      && this.state.password != ''
+    )
+    {
+      try {
+        let response = await fetch(SignUpUrl, {
+          method : 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            registration:{
+              email                : this.state.email,
+              password             : this.state.password,
+              password_confirmation: this.state.password_confirmation
+            }
+          })
+        });
+        if(response.status==200){
+          this.props.navigation.navigate("SignedOut");
+        }
+      }
+      catch(error) {
+        
+      }
+    }
   }
   render() {
     return (
       <View>
         <Button
-          title="SignedIn"
-          onPress={() => this.props.navigation.navigate("SignedIn")}
+          title   = "SignedIn"
+          onPress = {() => this.props.navigation.navigate("SignedIn")}
         />
         <Text>
-          Email:
+          Email: 
         </Text>
         <TextInput
-          value = {this.state.email}
-          onChangeText={(email) => this.setState({email})}
+          value        = {this.state.email}
+          onChangeText = {(email) => this.setState({email})}
         />
         <Text>
-          password:
+          password: 
         </Text>
         <TextInput
-          value = {this.state.password}
-          onChangeText={(password) => this.setState({password})}
+          value           = {this.state.password}
+          secureTextEntry = {true}
+          onChangeText    = {(password) => this.setState({password})}
         />
         <Text>
-          Re-password:
+          Re-password: 
         </Text>
         <TextInput
-          value = {this.state.password_confirmation}
-          onChangeText={(password_confirmation) => this.setState({password_confirmation})}
+          value           = {this.state.password_confirmation}
+          secureTextEntry = {true}
+          onChangeText    = {(password_confirmation) => this.setState({password_confirmation})}
         />
         <Button
-          title="Sign up"
-          onPress={() => this.ClickSignIn()}
+          title   = "Sign up"
+          onPress = {() => this.SignIn()}
         />
       </View>
       
     );
   }
-  async ClickSignIn(){
-    if(
-      this.state.password==this.state.password_confirmation 
-      && this.state.email!=''
-      && this.state.password!=''
-    )
-    {
-    try {
-      let response = await fetch('https://mysterious-shore-50693.herokuapp.com/api/v1/users/sign_up', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          registration:{
-            email                 : this.state.email,
-            password              : this.state.password,
-            password_confirmation :this.state.re_password
-          }
-        })
-      });
-      if(response.status==200){
-        this.props.navigation.navigate("SignedOut");
-      }
-      
-    } catch(error) {
-      
-    }
-  }
-  
-  }
-  
 }
 
 
