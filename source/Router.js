658357b09822d1c46762c87949bbc7d3f8e52ee6
Phathdt <phathdt.leviateam@gmail.com>
2017-11-22 00:58:33 +0700

    done split 3 segments

--- source/Router.js
+++ source/Router.js
@@ -1,38 +1,37 @@
-import { Image, StyleSheet } from 'react-native';
-import React, { Component } from 'react';
-import { TabNavigator } from 'react-navigation'
-import { StackNavigator } from 'react-navigation'
+import { Image, StyleSheet } from "react-native";
+import React, { Component } from "react";
+import { StackNavigator, TabNavigator } from "react-navigation";
 
 // Signed Out
-import SignIn from './Screens/SignedOut/SignIn'
-import SignUp from './Screens/SignedOut/SignUp'
+import SignIn from "./Screens/SignedOut/SignIn";
+import SignUp from "./Screens/SignedOut/SignUp";
 
 // Conversation
-import AddNewConversation from './Screens/Conversation/AddNewConversation'
-import DetailConversation from './Screens/Conversation/DetailConversation'
-import InformationConversation from './Screens/Conversation/InformationConversation'
-import ListConversation from './Screens/Conversation/ListConversation'
-import UpdateConversation from './Screens/Conversation/UpdateConversation'
+import AddNewConversation from "./Screens/Conversation/AddNewConversation";
+import DetailConversation from "./Screens/Conversation/DetailConversation";
+import InformationConversation from "./Screens/Conversation/InformationConversation";
+import ListConversation from "./Screens/Conversation/ListConversation";
+import UpdateConversation from "./Screens/Conversation/UpdateConversation";
 
 // Signed In
-import Home from './Screens/SignedIn/HomeStack/Home'
-import ListCoursePost from './Screens/SignedIn/HomeStack/ListCoursePost'
-import ListTeacherPost from './Screens/SignedIn/HomeStack/ListTeacherPost'
-import ListProfile from './Screens/SignedIn/HomeStack/ListProfile'
-import AddCoursePost from './Screens/SignedIn/HomeStack/AddCoursePost'
-import DetailCoursePost from './Screens/SignedIn/HomeStack/DetailCoursePost'
-import AddTeacherPost from './Screens/SignedIn/HomeStack/AddTeacherPost'
-import DetailTeacherPost from './Screens/SignedIn/HomeStack/DetailCoursePost'
+import Home from "./Screens/SignedIn/HomeStack/Home";
+import ListCoursePost from "./Screens/SignedIn/HomeStack/ListCoursePost";
+import ListTeacherPost from "./Screens/SignedIn/HomeStack/ListTeacherPost";
+import ListProfile from "./Screens/SignedIn/HomeStack/ListProfile";
+import AddCoursePost from "./Screens/SignedIn/HomeStack/AddCoursePost";
+import DetailCoursePost from "./Screens/SignedIn/HomeStack/DetailCoursePost";
+import AddTeacherPost from "./Screens/SignedIn/HomeStack/AddTeacherPost";
+import DetailTeacherPost from "./Screens/SignedIn/HomeStack/DetailCoursePost";
 
-import FilterCoursePost from './Screens/SignedIn/SearchStack/FilterCoursePost'
-import FilterTeacherPost from './Screens/SignedIn/SearchStack/FilterTeacherPost'
-import SearchProfile from './Screens/SignedIn/SearchStack/SearchProfile'
+import FilterCoursePost from "./Screens/SignedIn/SearchStack/FilterCoursePost";
+import FilterTeacherPost from "./Screens/SignedIn/SearchStack/FilterTeacherPost";
+import SearchProfile from "./Screens/SignedIn/SearchStack/SearchProfile";
 
-import Location from './Screens/SignedIn/LocationStack/Location'
+import Location from "./Screens/SignedIn/LocationStack/Location";
 
-import Profile from './Screens/SignedIn/ProfileStack/Profile'
+import Profile from "./Screens/SignedIn/ProfileStack/Profile";
 
-import styles from './src/stylesheet/style'
+import styles from "./src/stylesheet/style";
 
 export const createRootNavigator = (signedIn = false) => {
   return StackNavigator(
@@ -67,32 +66,32 @@ export const createRootNavigator = (signedIn = false) => {
 export const Conversation = StackNavigator({
   ListConversation: {
     screen: ListConversation,
-    navigationOptions: ({navigation}) => ({
-      title: `ListConversation`,
+    navigationOptions: ({ navigation }) => ({
+      title: `ListConversation`
     })
   },
   DetailConversation: {
     screen: DetailConversation,
-    navigationOptions: ({navigation}) => ({
-      title: `DetailConversation`,
+    navigationOptions: ({ navigation }) => ({
+      title: `DetailConversation`
     })
   },
   InformationConversation: {
     screen: InformationConversation,
-    navigationOptions: ({navigation}) => ({
-      title: `InformationConversation`,
+    navigationOptions: ({ navigation }) => ({
+      title: `InformationConversation`
     })
   },
   AddNewConversation: {
     screen: AddNewConversation,
-    navigationOptions: ({navigation}) => ({
-      title: `AddNewConversation`,
+    navigationOptions: ({ navigation }) => ({
+      title: `AddNewConversation`
     })
   },
   UpdateConversation: {
     screen: UpdateConversation,
-    navigationOptions: ({navigation}) => ({
-      title: `UpdateConversation`,
+    navigationOptions: ({ navigation }) => ({
+      title: `UpdateConversation`
     })
   }
 });
@@ -112,139 +111,112 @@ export const SignedOut = StackNavigator({
   }
 });
 
-export const SignedIn = TabNavigator({
-  HomeStack: {
-    screen: StackNavigator({
-      Home: { 
-        screen: Home,
-        navigationOptions: ({navigation}) => ({
-          title: `Home`,
-        })
-      },
-      ListCoursePost: { 
-        screen: ListCoursePost,
-        navigationOptions: ({navigation}) => ({
-          title: `ListCoursePost`,
-        })
-      },
-      DetailCoursePost: { 
-        screen: DetailCoursePost,
-        navigationOptions: ({navigation}) => ({
-          title: `DetailCoursePost`,
-        })
-      },
-      AddCoursePost: { 
-        screen: AddCoursePost,
-        navigationOptions: ({navigation}) => ({
-          title: `AddCoursePost`,
-        })
-      },
-      ListTeacherPost: { 
-        screen: ListTeacherPost,
-        navigationOptions: ({navigation}) => ({
-          title: `ListTeacherPost`,
-        })
-      },
-      DetailTeacherPost: { 
-        screen: DetailTeacherPost,
-        navigationOptions: ({navigation}) => ({
-          title: `DetailTeacherPost`,
-        })
-      },
-      AddTeacherPost: { 
-        screen: AddTeacherPost,
-        navigationOptions: ({navigation}) => ({
-          title: `AddTeacherPost`,
-        })
-      },
-      ListProfile: { 
-        screen: ListProfile,
-        navigationOptions: ({navigation}) => ({
-          title: `ListProfile`,
-        })
-      }
-    }),
-    navigationOptions: ({navigation}) => ({
-      tabBarLabel: `Home`,
-      tabBarIcon: ({ tintColor }) => (
-        <Image
-          source={require('./src/images/home.png')}
-          style={[styles.icon, {tintColor: tintColor}]}
-        />
-      ),
-    })
+export const SignedIn = TabNavigator(
+  {
+    HomeStack: {
+      screen: StackNavigator({
+        Home: {
+          screen: Home,
+          headerMode: "none",
+          header: null,
+          navigationOptions: ({ navigation }) => ({
+            header: null
+          })
+        },
+        DetailCoursePost: {
+          screen: DetailCoursePost
+        },
+        AddCoursePost: {
+          screen: AddCoursePost
+        },
+        DetailTeacherPost: {
+          screen: DetailTeacherPost
+        },
+        AddTeacherPost: {
+          screen: AddTeacherPost
+        }
+      }),
+      navigationOptions: ({ navigation }) => ({
+        tabBarLabel: `Home`,
+        tabBarIcon: ({ tintColor }) => (
+          <Image
+            source={require("./src/images/home.png")}
+            style={[styles.icon, { tintColor: tintColor }]}
+          />
+        )
+      })
+    },
+    SearchStack: {
+      screen: StackNavigator({
+        FilterCoursePost: {
+          screen: FilterCoursePost,
+          navigationOptions: ({ navigation }) => ({
+            title: `FilterCoursePost`
+          })
+        },
+        FilterTeacherPost: {
+          screen: FilterTeacherPost,
+          navigationOptions: ({ navigation }) => ({
+            title: `FilterTeacherPost`
+          })
+        },
+        SearchProfile: {
+          screen: SearchProfile,
+          navigationOptions: ({ navigation }) => ({
+            title: `SearchProfile`
+          })
+        }
+      }),
+      navigationOptions: ({ navigation }) => ({
+        tabBarLabel: `Home`,
+        tabBarIcon: ({ tintColor }) => (
+          <Image
+            source={require("./src/images/search.png")}
+            style={[styles.icon, { tintColor: tintColor }]}
+          />
+        )
+      })
+    },
+    LocationStack: {
+      screen: StackNavigator({
+        Location: {
+          screen: Location,
+          navigationOptions: ({ navigation }) => ({
+            title: `Location`
+          })
+        }
+      }),
+      navigationOptions: ({ navigation }) => ({
+        tabBarLabel: `Home`,
+        tabBarIcon: ({ tintColor }) => (
+          <Image
+            source={require("./src/images/location.png")}
+            style={[styles.icon, { tintColor: tintColor }]}
+          />
+        )
+      })
+    },
+    Profile: {
+      screen: Profile,
+      navigationOptions: ({ navigation }) => ({
+        tabBarLabel: `Profile`,
+        tabBarIcon: ({ tintColor }) => (
+          <Image
+            source={require("./src/images/profile.png")}
+            style={[styles.icon, { tintColor: tintColor }]}
+          />
+        )
+      })
+    }
   },
-  SearchStack: {
-    screen: StackNavigator({
-      FilterCoursePost: { 
-        screen: FilterCoursePost,
-        navigationOptions: ({navigation}) => ({
-          title: `FilterCoursePost`,
-        })
-      },
-      FilterTeacherPost: { 
-        screen: FilterTeacherPost,
-        navigationOptions: ({navigation}) => ({
-          title: `FilterTeacherPost`,
-        })
-      },
-      SearchProfile: { 
-        screen: SearchProfile,
-        navigationOptions: ({navigation}) => ({
-          title: `SearchProfile`,
-        })
-      },
-    }),
-    navigationOptions: ({navigation}) => ({
-      tabBarLabel: `Home`,
-      tabBarIcon: ({ tintColor }) => (
-        <Image
-          source={require('./src/images/search.png')}
-          style={[styles.icon, {tintColor: tintColor}]}
-        />
-      ),
-    })
-  },
-  LocationStack: {
-    screen: StackNavigator({
-      Location: { 
-        screen: Location,
-        navigationOptions: ({navigation}) => ({
-          title: `Location`,
-        })
-      }
-    }),
-    navigationOptions: ({navigation}) => ({
-      tabBarLabel: `Home`,
-      tabBarIcon: ({ tintColor }) => (
-        <Image
-          source={require('./src/images/location.png')}
-          style={[styles.icon, {tintColor: tintColor}]}
-        />
-      ),
-    })
-  },
-  Profile: {
-    screen: Profile,
-    navigationOptions: ({navigation}) => ({
-      tabBarLabel: `Profile`,
-      tabBarIcon: ({ tintColor }) => (
-        <Image
-          source={require('./src/images/profile.png')}
-          style={[styles.icon, {tintColor: tintColor}]}
-        />
-      ),
-    })
+  {
+    tabBarPosition: "bottom",
+    animationEnabled: true,
+    tabBarOptions: {
+      showIcon: true,
+      showLabel: false,
+      activeTintColor: "#13877A",
+      inactiveTintColor: "#696969"
+    }
   }
-},{
-  tabBarPosition: 'bottom',
-  animationEnabled: true,
-  tabBarOptions: {
-    showIcon: true,
-    showLabel: false,
-    activeTintColor: '#e3c800',
-    inactiveTintColor: '#a0522d'
-  },
-});
-
-
+);
--- source/Screens/SignedIn/HomeStack/Home.js
+++ source/Screens/SignedIn/HomeStack/Home.js
@@ -1,68 +1,146 @@
 import React, { Component } from "react";
+import {
+  Container,
+  Header,
+  Left,
+  Body,
+  Right,
+  Button,
+  Icon,
+  Title,
+  Segment,
+  Content,
+  Text
+} from "native-base";
+import { StackNavigator } from "react-navigation";
 
-import { View, Button, AsyncStorage, Text } from "react-native";
+import ListCoursePost from "./ListCoursePost";
+import AddCoursePost from "./AddCoursePost";
+import DetailCoursePost from "./DetailCoursePost";
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      token: null
+      activeSegment: "Course"
     };
-    this.takeToken();
   }
-
-  async takeToken() {
-    try {
-      let value = await AsyncStorage.getItem("Token");
-      if (value !== null) {
-        this.setState({
-          token: value
-        });
-      }
-    } catch (error) {}
+  renderCom() {
+    switch (this.state.activeSegment) {
+      case "Teacher":
+        return (
+          <Container>
+            <Text>Teacher</Text>
+          </Container>
+        );
+      case "Course":
+        return (
+          <Container style={{ flex: 1 }}>
+            <ListCoursePost navigation={this.props.navigation} />
+          </Container>
+        );
+      case "Profile":
+        return (
+          <Container>
+            <Text>Profile</Text>
+          </Container>
+        );
+    }
   }
 
   render() {
     return (
-      <View>
-        <Button
-          title="Conversation"
-          onPress={() => this.props.navigation.navigate("Conversation")}
-        />
-        <Button
-          title="Profile"
-          onPress={() => this.props.navigation.navigate("Profile")}
-        />
-        <Button
-          title="ListProfile"
-          onPress={() => this.props.navigation.navigate("ListProfile")}
-        />
-        <Button
-          title="ListCoursePost"
-          onPress={() => this.props.navigation.navigate("ListCoursePost")}
-        />
-        <Button
-          title="DetailCoursePost"
-          onPress={() => this.props.navigation.navigate("DetailCoursePost")}
-        />
-        <Button
-          title="AddCoursePost"
-          onPress={() => this.props.navigation.navigate("AddCoursePost")}
-        />
-        <Button
-          title="ListTeacherPost"
-          onPress={() => this.props.navigation.navigate("ListTeacherPost")}
-        />
-        <Button
-          title="DetailTeacherPost"
-          onPress={() => this.props.navigation.navigate("DetailTeacherPost")}
-        />
-        <Button
-          title="AddTeacherPost"
-          onPress={() => this.props.navigation.navigate("AddTeacherPost")}
-        />
-      </View>
+      <Container>
+        <Header style={{ backgroundColor: "white" }}>
+          <Segment style={{ backgroundColor: "white" }}>
+            <Button
+              first
+              style={{
+                backgroundColor:
+                  this.state.activeSegment === "Course" ? "#578F86" : "#FFFFFF",
+                borderColor: "#578F86"
+              }}
+              active={this.state.activeSegment === "Course"}
+              onPress={() => this.setState({ activeSegment: "Course" })}
+            >
+              <Text
+                style={{
+                  color:
+                    this.state.activeSegment === "Course"
+                      ? "#FFFFFF"
+                      : "#000000"
+                }}
+              >
+                Course
+              </Text>
+            </Button>
+            <Button
+              style={{
+                backgroundColor:
+                  this.state.activeSegment === "Teacher"
+                    ? "#578F86"
+                    : "#FFFFFF",
+                borderColor: "#578F86"
+              }}
+              active={this.state.activeSegment === "Teacher"}
+              onPress={() => this.setState({ activeSegment: "Teacher" })}
+            >
+              <Text
+                style={{
+                  color:
+                    this.state.activeSegment === "Teacher"
+                      ? "#FFFFFF"
+                      : "#000000"
+                }}
+              >
+                Teacher
+              </Text>
+            </Button>
+
+            <Button
+              last
+              style={{
+                backgroundColor:
+                  this.state.activeSegment === "Profile"
+                    ? "#578F86"
+                    : "#FFFFFF",
+                borderColor: "#578F86"
+              }}
+              active={this.state.activeSegment === "Profile"}
+              onPress={() => this.setState({ activeSegment: "Profile" })}
+            >
+              <Text
+                style={{
+                  color:
+                    this.state.activeSegment === "Profile"
+                      ? "#FFFFFF"
+                      : "#000000"
+                }}
+              >
+                Profile
+              </Text>
+            </Button>
+          </Segment>
+        </Header>
+        <Content padder>{this.renderCom()}</Content>
+      </Container>
     );
+  }
+}
+
+class Course extends Component {
+  render() {
+    <Container>
+      <Text>Course</Text>
+    </Container>;
+  }
+}
+
+class Profile extends Component {
+  render() {
+    <Container>
+      <Text>Profile</Text>
+    </Container>;
   }
 }
